58f01f7be4b16dc49f837db41f5756561a7e857d
xvimnt <jvmonteros98@gmail.com>
2020-08-06 12:00:02 -0600

    added fonts

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,28 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Text('Smart Mailbox App'),
-));
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Smart Mailbox'),
+          centerTitle: true,
+          backgroundColor: Colors.blue[300],
+        ),
+        body: Center(
+          child: Text(
+            'Hello world',
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 2,
+              color: Colors.grey[400],
+              fontFamily: 'QuickSand',
+            ),
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Text('click'),
+          onPressed: () {},
+          backgroundColor: Colors.blue[300],
+        ),
+      ),
+    ));
