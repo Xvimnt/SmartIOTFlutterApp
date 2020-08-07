9671a8e706278e9b5f6424c8b8e1734e1680130b
xvimnt <jvmonteros98@gmail.com>
2020-08-07 13:00:06 -0600

    added Icons

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,57 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Smart Mailbox'),
-          centerTitle: true,
-          backgroundColor: Colors.blue[300],
-        ),
-        body: Center(
-          child: Text(
-            'Hello world',
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 2,
-              color: Colors.grey[400],
-              fontFamily: 'QuickSand',
-            ),
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          child: Text('click'),
-          onPressed: () {},
-          backgroundColor: Colors.blue[300],
-        ),
+void main() => runApp(MaterialApp(home: Home()));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Smart Mailbox'),
+        centerTitle: true,
+        backgroundColor: Colors.blue[300],
       ),
-    ));
+      backgroundColor: Colors.blue[50],
+      body: Column(
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Column(
+                    children: <Widget>[Icon(Icons.mail_outline), Text('vacio')],
+                  )
+                ],
+              ),
+              Row(
+                children: <Widget>[
+                  Column(
+                    children: <Widget>[Icon(Icons.line_weight), Text('peso')],
+                  )
+                ],
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Column(
+                    children: <Widget>[Icon(Icons.opacity), Text('lleno')],
+                  )
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.info),
+        onPressed: () {},
+        backgroundColor: Colors.blue[300],
+      ),
+    );
+  }
+}
