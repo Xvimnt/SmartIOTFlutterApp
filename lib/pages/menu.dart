1e4618a64a8e7ef67fcbcfb238c083ab9bde91c5
xvimnt <jvmonteros98@gmail.com>
2020-08-11 13:29:33 -0600

    Added parameters

---
+++ lib/pages/menu.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  bool hasSomething;
+  int weight, waterLevel;
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Buzon Inteligente'),
+        centerTitle: true,
+        backgroundColor: Colors.blue[300],
+      ),
+      backgroundColor: Colors.blue[50],
+      body: Padding(
+        padding: EdgeInsets.symmetric(vertical: 100, horizontal: 25),
+        child: Column(
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    Column(
+                      children: <Widget>[
+                        Icon(
+                          Icons.mail_outline,
+                          size: 100,
+                        ),
+                        Text('vacio')
+                      ],
+                    )
+                  ],
+                ),
+                Row(
+                  children: <Widget>[
+                    Column(
+                      children: <Widget>[
+                        Icon(
+                          Icons.line_weight,
+                          size: 100,
+                        ),
+                        Text('peso')
+                      ],
+                    )
+                  ],
+                ),
+              ],
+            ),
+            Padding(
+              padding: EdgeInsets.all(50),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      Column(
+                        children: <Widget>[
+                          Icon(
+                            Icons.opacity,
+                            size: 100,
+                          ),
+                          Text('lleno')
+                        ],
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.info),
+        onPressed: () {},
+        backgroundColor: Colors.blue[300],
+      ),
+    );
+  }
+}
